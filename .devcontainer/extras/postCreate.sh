59b8974bb69578eaf3bc07a965a7dfc3ab4bc8d4
Héctor Flores <frhectorin@gmail.com>
2026-01-08 10:36:59 +0100

    Add files via upload

---
+++ .devcontainer/extras/postCreate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#######
+# Config ZSH
+#######
+# powerline fonts for zsh theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh plugins
+zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k'
+cp .devcontainer/extras/.zshrc ~
+cp .devcontainer/extras/.p10k.zsh ~
+
+git config --global --add safe.directory /home/vscode/hpc_unitn
