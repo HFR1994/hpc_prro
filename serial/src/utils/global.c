7ba1e143f14158942409871898f4dc849549deed
Hector Flores <frhectorin@gmail.com>
2026-01-12 13:19:57 +0000

    First Speedup

--- serial/src/utils/global.c
+++ serial/src/utils/global.c
@@ -1,4 +1,9 @@
 #include "utils/global.h"
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <strings.h>
+
 #include "utils/logger.h"
 
 void check_bounds(double *X, const int pop_size, const int features, const double lower_bound, const double upper_bound) {
@@ -14,4 +19,30 @@ void check_bounds(double *X, const int pop_size, const int features, const doubl
             X[i] = upper_bound;
         }
     }
+}
+
+bool str_to_bool(const char *s, const bool default_value) {
+    if (s == NULL || *s == '\0') {
+        return default_value;
+    }
+
+    // Skip leading whitespace
+    while (isspace((unsigned char)*s)) s++;
+
+    if (!strcasecmp(s, "1") ||
+        !strcasecmp(s, "true") ||
+        !strcasecmp(s, "yes") ||
+        !strcasecmp(s, "on")) {
+        return true;
+        }
+
+    if (!strcasecmp(s, "0") ||
+        !strcasecmp(s, "false") ||
+        !strcasecmp(s, "no") ||
+        !strcasecmp(s, "off")) {
+        return false;
+        }
+
+    // Fallback
+    return default_value;
 }
